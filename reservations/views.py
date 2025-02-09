ca41e715da63c9061381285217bf87a12d8bcc5f
Atakan Karaca <atakan.karaca@metu.edu.tr>
2025-02-09 10:29:10 +0300

    implement occupancy check

--- reservations/views.py
+++ reservations/views.py
@@ -7,6 +7,11 @@ from rest_framework import status
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    def create(self, request, *args, **kwargs):
+        flight = self.get_object().flight
+        if flight.reservation_set.count() >= flight.airplane.capacity:
+            return Response({"message": "Flight is full"}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
